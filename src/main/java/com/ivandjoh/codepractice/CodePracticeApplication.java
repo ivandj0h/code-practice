a01863d8b7bfe08a277fb7c2f8ee922f0ea5cb19
ivandjoh <ivandi.djoh@gmail.com>
2023-01-14 17:47:20 +0800

    init

---
+++ src/main/java/com/ivandjoh/codepractice/CodePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.ivandjoh.codepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodePracticeApplication.class, args);
+	}
+
+}
