81d10f69d4b232b6690a8852db23bf8ab05f19a8
ivandjoh <ivandi.djoh@gmail.com>
2023-01-14 18:04:18 +0800

    Added fancy number

--- src/main/java/com/ivandjoh/codepractice/App.java
+++ src/main/java/com/ivandjoh/codepractice/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CodePracticeApplication {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CodePracticeApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 
 }
---
+++ src/main/java/com/ivandjoh/codepractice/fancyNumber/FancyNumber.java
@@ -0,0 +1,26 @@
+package com.ivandjoh.codepractice.fancyNumber;
+
+/**
+ * @author Ivan Djoh
+ * @version 1.0
+ * @since 2023-01-14
+
+ * Given a mobile number and some conditions for a fancy number,
+ * find if the given number is fancy. A 10-digit mobile number is called fancy
+ * if it satisfies any of the following three conditions.
+ *  - A single number occurs three consecutive times. Like 777.
+ * Three consecutive digits are either in increasing or decreasing fashion. Like 456 or 987.
+ *  - A single digit occurs four or more times in the number. Like 9859009976 – here the digit 9
+ *  occurs 4 times.
+
+ *  Example:
+ *  Input:  9859009976
+ *  Output: yes
+ *  The given mobile number satisfies condition three given above.
+
+ *  input:  7609438921
+ *  output: no
+ *  The given three conditions satisfy
+ */
+public class FancyNumber {
+}
