013a1202ca2b9e88452fb96c6908a013d2901ddb
ivandjoh <ivandi.djoh@gmail.com>
2023-01-16 17:24:21 +0800

    Find The Missing Number

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ This repository contains code practice for the following languages: ```JAVA```
 - Fancy Numbers
 - Validate Mobile Number
 - Find Largest of Three Elements
+- Find The Missing Number
---
+++ src/main/java/com/ivandjoh/codepractice/Array/FindTheMissingNumber.java
@@ -0,0 +1,46 @@
+package com.ivandjoh.codepractice.Array;
+
+/**
+ * @author Ivan Djoh
+ * @version 1.0
+ * @since 2023-01-16
+
+ * Given an array arr[] of size N-1 with integers in the range of [1, N],
+ * the task is to find the missing number from the first N integers.
+ * Note: There are no duplicates in the list.
+
+ * Example:
+ * Input: arr[] = {1, 2, 4, 6, 3, 7, 8}, N = 8
+ * Output: 5
+ *
+ * Explanation: The missing number from 1 to 8 is 5.
+ */
+public class FindTheMissingNumber {
+
+    public static void getMissingNo(int[] arr, int size) {
+        int i;
+
+        for (i = 0; i < size; i++) {
+            if (Math.abs(arr[i]) - 1 == size) {
+                continue;
+            }
+            int ind = Math.abs(arr[i]) - 1;
+            arr[ind] *= -1;
+        }
+
+        int ans = size + 1;
+        for (i = 0; i < size; i++) {
+            if (arr[i] > 0)
+                ans = i + 1;
+        }
+        System.out.println(ans);
+    }
+
+    public static void executeFindTheMissingNumber() {
+        int arr[] = { 1, 3, 7, 5, 6, 2 };
+        int n = arr.length;
+
+        // Function call
+        getMissingNo(arr, n);
+    }
+}
