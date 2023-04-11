ef616a05552c0d31e5ca80f40da01cf035ceab8e
ivandjoh <ivandi.djoh@gmail.com>
2023-04-11 15:41:13 +0800

    Added Binary String Conversion

---
+++ src/main/java/com/ivandjoh/codepractice/Search/BinaryStringConversion.java
@@ -0,0 +1,15 @@
+package com.ivandjoh.codepractice.Search;
+
+public class BinaryStringConversion {
+
+    public static void executeBinaryStringConversion(String args) {
+        int N = 9;
+        binaryStringConversion(N);
+    }
+
+    private static int binaryStringConversion(int N) {
+        String binaryString = Integer.toBinaryString(N);
+        System.out.println("Binary String: " + binaryString);
+        return 0;
+    }
+}
